bcb9d2e7d136df0222708671044fd5e150819406
Amr Koritem <amr.koritem92@gmail.com>
2023-02-13 10:51:28 +0200

    Initial Commit

---
+++ AKUpdateManager/AKUpdateManager.h
@@ -0,0 +1,18 @@
+//
+//  AKUpdateManager.h
+//  AKUpdateManager
+//
+//  Created by Amr Koritem on 13/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AKUpdateManager.
+FOUNDATION_EXPORT double AKUpdateManagerVersionNumber;
+
+//! Project version string for AKUpdateManager.
+FOUNDATION_EXPORT const unsigned char AKUpdateManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AKUpdateManager/PublicHeader.h>
+
+
